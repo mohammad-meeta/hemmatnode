a1c1e6530603b64e84324742f0dc0368fc834253
kamal jamali <kamal.jamali63@gmail.com>
2020-04-23 18:19:56 +0430

    update user post 	password required and send from frontend

--- validators/user-register-validator.js
+++ validators/user-register-validator.js
@@ -6,7 +6,7 @@ const Validator = require('validatorjs');
 /**
  * User Login Validator
  */
-function UserRegisterValidator() { }
+function UserRegisterValidator() {}
 module.exports = UserRegisterValidator;
 
 UserRegisterValidator.async = true;
@@ -25,6 +25,7 @@ UserRegisterValidator.validate = function validate(req, res, next) {
 UserRegisterValidator.data = function data(req) {
     return {
         name: req.body.name,
+        password: req.body.password,
         email: req.body.email,
         first_name: req.body.first_name,
         last_name: req.body.last_name,
@@ -39,6 +40,7 @@ UserRegisterValidator.data = function data(req) {
 UserRegisterValidator.rules = function rules() {
     return {
         'name': 'required|min:3|max:25|user_name_available',
+        'password': 'required|size:6',
         'email': 'required|email|email_available',
         'first_name': 'required|min:3|max:25',
         'last_name': 'required|min:4|max:25',
@@ -60,6 +62,7 @@ UserRegisterValidator.messages = function messages() {
 UserRegisterValidator.attributes = function attributes() {
     return {
         name: 'user_name',
+        password: 'password',
         email: 'email',
         first_name: 'first_name',
         last_name: 'last_name',

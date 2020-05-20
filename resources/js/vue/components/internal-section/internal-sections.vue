5130b6221611ce75dc4ab45c319c2973e5584410
Kamal <kamal.jamali63@gmail.com>
2020-05-20 12:51:23 +0430

    add department in internal section

--- resources/js/vue/components/internal-section/internal-sections.vue
+++ resources/js/vue/components/internal-section/internal-sections.vue
@@ -15,8 +15,13 @@
                         .control
                             a.button.is-primary.is-rounded(href="/result") برآمدها
             .container
-                .intro-card.p-3(v-for='internalSection in internalSections', :key='internalSection.id')
-                    .intro-card-head {{ internalSection.title }}
+                .columns
+                    .column.is-8
+                        .intro-card.p-3(v-for='internalSection in internalSections', :key='internalSection.id')
+                            .intro-card-head {{ internalSection.title }}
+                    .column
+                        .intro-card.p-3(v-for='dep in department', :key='dep.id')
+                            .intro-card-head {{ dep.title }}
 </template>
 
 <script>
@@ -29,108 +34,117 @@ const Loading = require("VUE-COMPONENTS/general/loading.vue").default;
 const Notification = require("VUE-COMPONENTS/general/notification.vue").default;
 
 module.exports = {
-    name: "InternalSection",
+  name: "InternalSection",
 
-    components: {
-        Loading,
-        Notification
+  components: {
+    Loading,
+    Notification
+  },
+
+  data: () => ({
+    ENUMS,
+    formModeStack: [],
+    internalSections: [],
+    department: [],
+    notificationMessage: null,
+    notificationType: "is-info"
+  }),
+
+  props: {
+    title: {
+      type: String,
+      default: null
     },
 
-    data: () => ({
-        ENUMS,
-        formModeStack: [],
-        internalSections: [],
-        notificationMessage: null,
-        notificationType: "is-info",
-    }),
-
-    props: {
-        title: {
-            type: String,
-            default: null
-        },
-
-        departmentCategoriesUrl: {
-            type: String,
-            default: null
-        }
+    departmentCategoriesUrl: {
+      type: String,
+      default: null
     },
 
-    computed: {
-        formMode: state => state.formModeStack[state.formModeStack.length - 1],
-
-        modeLoading: state => state.formMode == ENUMS.FORM_MODE.LOADING,
-        modeList: state => state.formMode == ENUMS.FORM_MODE.LIST,
-        showNotification: state => state.notificationMessage != null
-    },
-
-    created() {
-        this.init();
-    },
-
-    mounted() {
-        this.changeFormMode(ENUMS.FORM_MODE.LIST);
-        this.loadInternalSections(1);
-    },
-
-    methods: {
-
-        /**
-         * Load internalSections
-         */
-        loadInternalSections(pageId) {
-            let url = this.departmentCategoriesUrl.replace("$page$", pageId);
-            url = url.replace("$pageSize$", 50);
-            AxiosHelper.send("get", url, "").then(res => {
-                const resData = res.data;
-                Vue.set(this, "internalSections", resData.data.data);
-            });
-        },
-
-        /**
-         * Init method
-         */
-        init() {
-            this.changeFormMode(ENUMS.FORM_MODE.LOADING);
-        },
-
-        /**
-         * Change form mode
-         */
-        changeFormMode(mode, options) {
-            const opts = Object.assign(
-                {
-                    pop: false
-                },
-                options
-            );
-
-            if (opts.pop) {
-                if (this.formModeStack.length > 0) {
-                    Vue.delete(
-                        this.formModeStack,
-                        this.formModeStack.length - 1
-                    );
-                }
-            } else {
-                Vue.set(this.formModeStack, this.formModeStack.length, mode);
-            }
-        },
-
-        /**
-         * Set notification
-         */
-        setNotification(message, notificationType = "is-info") {
-            Vue.set(this, "notificationType", notificationType);
-            Vue.set(this, "notificationMessage", message);
-        },
-
-        /**
-         * Close Notification
-         */
-        closeNotification() {
-            this.setNotification(null);
-        }
+    departmentUrl: {
+      type: String,
+      default: null
     }
+  },
+
+  computed: {
+    formMode: state => state.formModeStack[state.formModeStack.length - 1],
+
+    modeLoading: state => state.formMode == ENUMS.FORM_MODE.LOADING,
+    modeList: state => state.formMode == ENUMS.FORM_MODE.LIST,
+    showNotification: state => state.notificationMessage != null
+  },
+
+  created() {
+    this.init();
+  },
+
+  mounted() {
+    this.changeFormMode(ENUMS.FORM_MODE.LIST);
+    this.loadInternalSections(1);
+  },
+
+  methods: {
+    /**
+     * Load internalSections
+     */
+    loadInternalSections(pageId) {
+      let url = this.departmentCategoriesUrl.replace("$page$", pageId);
+      url = url.replace("$pageSize$", 50);
+      AxiosHelper.send("get", url, "").then(res => {
+        const resData = res.data;
+        Vue.set(this, "internalSections", resData.data.data);
+      });
+
+      url = this.departmentUrl.replace("$page$", pageId);
+      url = url.replace("$pageSize$", 50);
+      AxiosHelper.send("get", url, "").then(res => {
+        const resData = res.data;
+        Vue.set(this, "department", resData.data.data);
+      });
+    },
+
+    /**
+     * Init method
+     */
+    init() {
+      this.changeFormMode(ENUMS.FORM_MODE.LOADING);
+    },
+
+    /**
+     * Change form mode
+     */
+    changeFormMode(mode, options) {
+      const opts = Object.assign(
+        {
+          pop: false
+        },
+        options
+      );
+
+      if (opts.pop) {
+        if (this.formModeStack.length > 0) {
+          Vue.delete(this.formModeStack, this.formModeStack.length - 1);
+        }
+      } else {
+        Vue.set(this.formModeStack, this.formModeStack.length, mode);
+      }
+    },
+
+    /**
+     * Set notification
+     */
+    setNotification(message, notificationType = "is-info") {
+      Vue.set(this, "notificationType", notificationType);
+      Vue.set(this, "notificationMessage", message);
+    },
+
+    /**
+     * Close Notification
+     */
+    closeNotification() {
+      this.setNotification(null);
+    }
+  }
 };
 </script>

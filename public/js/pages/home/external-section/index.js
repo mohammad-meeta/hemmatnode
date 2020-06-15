16baf77b32d8c664fc58cfac6a771fd7837b8f26
Kamal <kamal.jamali63@gmail.com>
2020-06-15 09:59:26 +0430

    change this codes

--- public/js/pages/home/external-section/index.js
+++ public/js/pages/home/external-section/index.js
@@ -962,7 +962,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/mohammad/Documents/Projects/olompezeshki/hemmatnode/resources/js/pages/home/external-section/index.js */"./resources/js/pages/home/external-section/index.js");
+module.exports = __webpack_require__(/*! /home/sources/hemmatnode/resources/js/pages/home/external-section/index.js */"./resources/js/pages/home/external-section/index.js");
 
 
 /***/ })

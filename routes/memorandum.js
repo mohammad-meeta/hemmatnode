d026b44a8220152b75784fd14552b4fd78a4db66
Kamal <kamal.jamali63@gmail.com>
2020-09-10 19:09:55 +0430

    update memorandum function

--- routes/memorandum.js
+++ routes/memorandum.js
@@ -16,66 +16,67 @@ const {
 } = use('core/helpers/auth-helper');
 
 Router.get('/memorandum/:department?', [
-        checkSession,
-        Rule.canAsync('user.permision', 'memorandum.index'),
-        'Memorandum@index'
-    ])
+    checkSession,
+    Rule.canAsync('user.permision', 'memorandum.index'),
+    'Memorandum@index'
+])
     .as('memorandum.index');
 
 Router.get('/api/memorandum/:group/:page/:size?/', [
-        checkSession,
-        'Memorandum@paginateMemorandum'
-    ])
+    checkSession,
+    'Memorandum@paginateMemorandum'
+])
     .as('api.memorandum');
 
 Router.get('/memorandum/create', [
-        checkSession,
-        Rule.canAsync('user.permision', 'memorandum.create'),
-        'Memorandum@create'
-    ])
+    checkSession,
+    Rule.canAsync('user.permision', 'memorandum.create'),
+    'Memorandum@create'
+])
     .as('memorandum.create');
 
 Router.get('/memorandum/edit', [
-        checkSession,
-        Rule.canAsync('user.permision', 'memorandum.edit'),
-        'Memorandum@edit'
-    ])
+    checkSession,
+    Rule.canAsync('user.permision', 'memorandum.edit'),
+    'Memorandum@edit'
+])
     .as('memorandum.edit');
 
 Router.post('/memorandum/:department?', [
-        upload.array('files'),
-        checkSession,
-        Rule.canAsync('user.permision', 'memorandum.store'),
-        validator.validate,
-        'Memorandum@store'
-    ])
+    upload.array('files'),
+    checkSession,
+    Rule.canAsync('user.permision', 'memorandum.store'),
+    validator.validate,
+    'Memorandum@store'
+])
     .as('memorandum.store');
 
 Router.get('/memorandum/:memorandum', [
-        checkSession,
-        Rule.canAsync('user.permision', 'memorandum.show'),
-        'Memorandum@show'
-    ])
+    checkSession,
+    Rule.canAsync('user.permision', 'memorandum.show'),
+    'Memorandum@show'
+])
     .as('memorandum.show');
 
 Router.get('/api/memorandum/:memorandum/edit', [
-        checkSession,
-        Rule.canAsync('user.permision', 'api.memorandum.edit'),
-        'Memorandum@editMemorandumData'
-    ])
+    checkSession,
+    Rule.canAsync('user.permision', 'api.memorandum.edit'),
+    'Memorandum@editMemorandumData'
+])
     .as('api.memorandum.edit');
 
 Router.patch('/memorandum/:id', [
-        checkSession,
-        Rule.canAsync('user.permision', 'memorandum.update'),
-        validator.validate,
-        'Memorandum@update'
-    ])
+    checkSession,
+    Rule.canAsync('user.permision', 'memorandum.update'),
+    validator.validate,
+    upload.array('files'),
+    'Memorandum@update'
+])
     .as('memorandum.update');
 
 Router.delete('/memorandum/:memorandum', [
-        checkSession,
-        Rule.canAsync('user.permision', 'memorandum.destroy'),
-        'Memorandum@destroy'
-    ])
+    checkSession,
+    Rule.canAsync('user.permision', 'memorandum.destroy'),
+    'Memorandum@destroy'
+])
     .as('memorandum.destroy');

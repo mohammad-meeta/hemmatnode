8729b6639a02be44c2d2273688ce57171bc7c52e
Kamal <kamal.jamali63@gmail.com>
2020-09-27 19:31:54 +0330

    add power

---
+++ app/controllers/PowerController.js
@@ -0,0 +1,330 @@
+'use strict';
+const PugView = use('app/helpers/pug-view');
+const PowerHelper = use('app/helpers/power-helper');
+const FileHelper = use('app/helpers/file-helper');
+/**
+ * Dep cat controller
+ */
+function Power() { }
+module.exports = Power;
+
+/**
+ * Index route
+ */
+Power.index = async function index(req, res, next) {
+    const pageRoute = 'power.index';
+
+    res.render(PugView.getView(pageRoute), {
+        req,
+        pageRoute,
+        departmentId: req.params.department,
+        year: req.params.year,
+    });
+};
+/**
+ * paginate route
+ */
+Power.paginatePower = async function paginatePower(req, res, next) {
+    const group = req.params.group;
+
+    const dataPaginate = {
+        page: req.params.page,
+        pageSize: req.params.size || 10
+    };
+
+    try {
+        let result = {};
+
+        let data = await PowerHelper.loadAllPowerCountData(group);
+        let count = data.data;
+
+        data = await PowerHelper.loadAllPowerData(req, dataPaginate, group);
+        result = {
+            success: true,
+            data: {
+                data: data,
+                count: count
+            }
+        };
+
+        res.status(200)
+            .send(result)
+            .end();
+
+    }
+    catch (err) {
+
+        Logger.error(err);
+
+        res.status(500)
+            .send(err)
+            .end();
+    }
+};
+/**
+ * paginate all route
+ */
+Power.paginatePowerAll = async function paginatePowerAll(req, res, next) {
+    const dataPaginate = {
+        page: req.params.page,
+        pageSize: req.params.size || 10
+    };
+
+    try {
+        let result = {};
+
+        let data = await PowerHelper.loadAllPowerCountDataAll();
+        let count = data.data;
+
+        data = await PowerHelper.loadAllPowerDataAll(req, dataPaginate);
+        result = {
+            success: true,
+            data: {
+                data: data,
+                count: count
+            }
+        };
+
+        res.status(200)
+            .send(result)
+            .end();
+
+    }
+    catch (err) {
+
+        Logger.error(err);
+
+        res.status(500)
+            .send(err)
+            .end();
+    }
+};
+/**
+ * paginate by year route
+ */
+Power.paginatePowerYear = async function paginatePowerYear(req, res, next) {
+    const group = req.params.group;
+    const year = req.params.year;
+
+    const dataPaginate = {
+        page: req.params.page,
+        pageSize: req.params.size || 10
+    };
+
+    try {
+        let result = {};
+
+        let data = await PowerHelper.loadAllPowerCountYearData(group, year);
+        let count = data.data;
+
+        data = await PowerHelper.loadAllPowerYearData(req, dataPaginate, group, year);
+        result = {
+            success: true,
+            data: {
+                data: data,
+                count: count
+            }
+        };
+
+        res.status(200)
+            .send(result)
+            .end();
+
+    }
+    catch (err) {
+
+        Logger.error(err);
+
+        res.status(500)
+            .send(err)
+            .end();
+    }
+};
+/**
+ * group date power
+ */
+Power.groupDate = async function groupDate(req, res, next) {
+    const group = req.params.group;
+
+    const data = await PowerHelper.loadGroupDate(req, group);
+    const result = {
+        success: true,
+        data: data
+    };
+
+    res.status(200)
+        .send(result)
+        .end();
+};
+
+/**
+ * show route
+ */
+/**
+ * load data with id
+ */
+Power.show = async function show(req, res, next) {
+    const Id = req.params.power;
+
+    PowerHelper.loadPowerData(Id)
+
+        .then(data => {
+            const result = {
+                success: true,
+                data: {
+                    data: data,
+                }
+            };
+            res.status(200)
+                .send(result)
+                .end();
+        })
+        .catch(err => console.error(err));
+};
+
+/**
+ * delete data dep cat
+ */
+Power.destroy = async function destroy(req, res, next) {
+    const data = {
+        "_id": req.body._id
+    };
+
+    PowerHelper.deletePowerData(data)
+        .then(data => {
+            const result = {
+                success: true,
+                data: data
+            };
+            res.status(200)
+                .send(result)
+                .end();
+        })
+        .catch(err => console.error(err));
+};
+
+/**
+ * Create route return page
+ */
+Power.create = async function create(req, res, next) {
+    const pageRoute = PugView.getView('power.create');
+
+    res.render(pageRoute, {
+        req,
+        pageRoute
+    });
+};
+
+/**
+ * store data
+ */
+Power.store = async function store(req, res, next) {
+    const files = req.files || [];
+    let fileList = [];
+
+    for (let i = 0; i < files.length; ++i) {
+        try {
+            const el = files[i];
+            el.user_id = req.session.auth.userId;
+
+            const data = await FileHelper.insertFileData(el);
+
+            const tempFileData = {
+                file_id: data[0]._id,
+                deleted_at: null,
+            };
+            fileList.push(tempFileData);
+        } catch (err) {
+            Logger.error(err);
+        }
+    }
+
+    const data = {
+        "title": req.body.title,
+        "description": req.body.description,
+        "date": req.body.date,
+        "user_id": req.session.auth.userId,
+        "is_active": req.body.is_active,
+        "department_id": req.body.department_id,
+        "files": fileList
+    };
+    console.log(data)
+    PowerHelper.insertNewPower(data)
+        .then(dataRes => {
+            const result = {
+                success: true,
+                data: dataRes
+            };
+            res.status(200)
+                .send(result)
+                .end();
+        })
+        .catch(err => console.error(err));
+};
+
+/**
+ * update data
+ */
+Power.update = async function update(req, res, next) {
+    let data = {};
+    const files = req.files || [];
+    let fileList = [];
+
+    for (let i = 0; i < files.length; ++i) {
+        try {
+            const el = files[i];
+            el.user_id = req.session.auth.userId;
+
+            const data = await FileHelper.insertFileData(el);
+
+            const tempFileData = {
+                file_id: data[0]._id,
+                deleted_at: null,
+            };
+            fileList.push(tempFileData);
+        } catch (err) {
+            Logger.error(err);
+        }
+    }
+
+    const deletedOldFiles = JSON.parse(req.body.deletedOldFiles || null) || [];
+
+    let powerRes = await PowerHelper.loadPowerData(req.body._id);
+    const powerLFiles = (powerRes || {}).files || [];
+
+    for (let index = 0; index < powerLFiles.length; index++) {
+        const element = powerLFiles[index];
+        fileList.push(element)
+    }
+
+    for (let index = 0; index < deletedOldFiles.length; index++) {
+        const element = deletedOldFiles[index];
+        for (let oil = 0; oil < fileList.length; oil++) {
+            const Fele = fileList[oil];
+            if (Fele.file_id == element) {
+                Fele.deleted_at = Date()
+            }
+        }
+    }
+
+    data = {
+        "_id": req.body._id,
+        "title": req.body.title,
+        "description": req.body.description,
+        "date": req.body.date,
+        "user_id": req.session.auth.userId,
+        "is_active": req.body.is_active,
+        "department_id": req.body.department_id,
+        "files": fileList
+    };
+
+    let result = await PowerHelper.updatePowerData(data);
+    const result2 = {
+        success: true,
+        data: result,
+    };
+
+    res.status(200)
+        .send(result2)
+        .end();
+};
---
+++ routes/power.js
@@ -0,0 +1,73 @@
+'use strict';
+const Rule = use('core/helpers/rule-helper');
+// const validator = use('validators/power-register-validator');
+const multer = require("multer");
+const upload = multer({
+    dest: "uploads/"
+});
+
+const {
+    checkSession
+} = use('app/helpers/auth-helper');
+
+const {
+    checkAuth,
+    clearAuth
+} = use('core/helpers/auth-helper');
+
+Router.get('/power/:department/:year?', [
+    checkSession,
+    Rule.canAsync('user.permision', 'power.index'),
+    'Power@index'
+])
+    .as('power.index');
+
+// Router.get("/api/powers/:page/:size?", [
+//     checkSession,
+//     Rule.canAsync("user.permision", "api.power"),
+//     "Power@paginatePowerAll"
+// ]).as("api.power.all");
+
+Router.get("/api/powers/:group/:page/:size?", [
+    checkSession,
+    Rule.canAsync("user.permision", "api.power"),
+    "Power@paginatePower"
+]).as("api.power");
+
+Router.get('/power/create', [
+    checkSession,
+    Rule.canAsync('user.permision', 'power.create'),
+    'Power@create'
+])
+    .as('power.create');
+
+Router.post('/power', [
+    upload.array('files'),
+    checkSession,
+    Rule.canAsync('user.permision', 'power.store'),
+    // validator.validate,
+    'Power@store'
+])
+    .as('power.store');
+
+Router.patch("/power/:id/edit", [
+    upload.array("files"),
+    checkSession,
+    Rule.canAsync("user.permision", "power.update"),
+    // validator.validate,
+    "Power@update"
+]).as("power.update");
+
+Router.get('/power/:power', [
+    checkSession,
+    Rule.canAsync('user.permision', 'power.show'),
+    'Power@show'
+])
+    .as('power.show');
+
+Router.delete('/power/:power', [
+    checkSession,
+    Rule.canAsync('user.permision', 'power.destroy'),
+    'Power@destroy'
+])
+    .as('power.destroy');

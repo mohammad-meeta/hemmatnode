ab4ef7bf0674f291214c1bed9c490bbbf3c61566
Kamal <kamal.jamali63@gmail.com>
2020-09-28 17:57:53 +0330

    change structure formaat in peaople

--- resources/js/vue/components/people-participation/people-participations.vue
+++ resources/js/vue/components/people-participation/people-participations.vue
@@ -1,33 +1,58 @@
 <template lang="pug">
-    .container-parent
-        section.hero
-            notification(:notification-type="notificationType",
-                         @on-close="closeNotification", v-if="showNotification")
-                span(v-html="notificationMessage")
+.container-parent
+    section.hero
+        notification(
+            :notification-type="notificationType",
+            @on-close="closeNotification",
+            v-if="showNotification"
+        )
+            span(v-html="notificationMessage")
 
-            .container.page-header
-                .title
-                    h1 مشارکت مردمی
-            .container.main-content
-                //.intro-cards.columns
-                    .column.is-4(v-for='departmentCategory in departmentCategories',
-                                 :key='departmentCategory.id')
-                        .intro-card
-                            .intro-card-head
-                                h2 {{ departmentCategory.title }}
-                            .panel-block.is-active(v-for='dep in departmentCategory.department', :key='dep._id')
-                                a(:href="getUrl(dep._id)") {{ dep.title }}
-
-                .inline-cards
-                    .inline-card.w-100(v-for='departmentCategory in departmentCategories',
-                                 :key='departmentCategory.id')
-                        .inline-card-head
+        .container.page-header
+            .title
+                h1 مشارکت مردمی
+        .container.main-content
+            //.intro-cards.columns
+                .column.is-4(v-for='departmentCategory in departmentCategories',
+                             :key='departmentCategory.id')
+                    .intro-card
+                        .intro-card-head
                             h2 {{ departmentCategory.title }}
-                        .inline-card-body
-                            .inline-card-body-item(v-for='dep in departmentCategory.department', :key='dep._id')
+                        .panel-block.is-active(v-for='dep in departmentCategory.department', :key='dep._id')
+                            a(:href="getUrl(dep._id)") {{ dep.title }}
+
+            .inline-cards
+                .inline-card.w-100(
+                    v-for="departmentCategory in departmentCategories",
+                    :key="departmentCategory.id"
+                )
+                    .inline-card-head
+                        h2 {{ departmentCategory.title }}
+                    .inline-card-body
+                        .inline-card-body-item(
+                            v-for="dep in departmentCategory.department",
+                            :key="dep._id"
+                        )
+                            //a(:href="getUrl(dep._id)")
+                            div(v-if="!hasChildren(dep)")
                                 a(:href="getUrl(dep._id)") {{ dep.title }}
+                            div(v-if="hasChildren(dep)")
+                                a(@click="showModal(dep)")
+                                    | {{ dep.title }}
+                                b-modal.departments-modal(
+                                    :active.sync="isModalActive"
+                                )
+                                    .childrens(
+                                        v-for="child in departmentChild"
+                                    )
+                                        a(:href="getUrl(child._id)") {{ child.title }}
 
-
+                                //b-collapse(:open='false', aria-id='contentIdForA11y1')
+                                    a(slot='trigger', aria-controls='contentIdForA11y1') {{ dep.title }}
+                                    .notification
+                                        .content
+                                            .childrens(v-for='child in dep.child')
+                                                a(:href="getUrl(child._id)") {{ child.title }}
 </template>
 
 <script>
@@ -38,13 +63,14 @@ const ENUMS = require("JS-HELPERS/enums");
 const Loading = require("VUE-COMPONENTS/general/loading.vue").default;
 
 const Notification = require("VUE-COMPONENTS/general/notification.vue").default;
+const Buefy = require("buefy").default;
 
 export default {
-    name: "PeopleParticipation",
+    name: "ExternalSection",
 
     components: {
         Loading,
-        Notification
+        Notification,
     },
 
     data: () => ({
@@ -53,32 +79,35 @@ export default {
         departmentCategories: [],
         department: [],
         notificationMessage: null,
-        notificationType: "is-info"
+        notificationType: "is-info",
+        isModalActive: false,
+        departmentChild: [],
     }),
 
     props: {
         title: {
             type: String,
-            default: null
+            default: null,
         },
 
         departmentLinkUrl: {
             type: String,
-            default: null
+            default: null,
         },
 
         departmentCategoriesUrl: {
             type: String,
-            default: null
-        }
+            default: null,
+        },
     },
 
     computed: {
-        formMode: state => state.formModeStack[state.formModeStack.length - 1],
-
-        modeLoading: state => state.formMode == ENUMS.FORM_MODE.LOADING,
-        modeList: state => state.formMode == ENUMS.FORM_MODE.LIST,
-        showNotification: state => state.notificationMessage != null
+        formMode: (state) =>
+            state.formModeStack[state.formModeStack.length - 1],
+        hasChild: (state) => state.data.department.child.length > 0,
+        modeLoading: (state) => state.formMode == ENUMS.FORM_MODE.LOADING,
+        modeList: (state) => state.formMode == ENUMS.FORM_MODE.LIST,
+        showNotification: (state) => state.notificationMessage != null,
     },
 
     created() {
@@ -101,11 +130,25 @@ export default {
         },
 
         /**
+         * set department link behavior
+         */
+        hasChildren(dep) {
+            return dep.child.length > 0;
+        },
+
+        showModal(dep) {
+            if (dep.child.length > 0) {
+                Vue.set(this, "departmentChild", dep.child);
+                this.isModalActive = true;
+            }
+        },
+
+        /**
          * Load departmentCategories
          */
         loadDepartmentCategories() {
             AxiosHelper.send("get", this.departmentCategoriesUrl, "").then(
-                res => {
+                (res) => {
                     const resData = res.data;
                     Vue.set(this, "departmentCategories", resData.data.data);
                 }
@@ -124,7 +167,7 @@ export default {
         changeFormMode(mode, options) {
             const opts = Object.assign(
                 {
-                    pop: false
+                    pop: false,
                 },
                 options
             );
@@ -154,7 +197,7 @@ export default {
          */
         closeNotification() {
             this.setNotification(null);
-        }
-    }
+        },
+    },
 };
 </script>

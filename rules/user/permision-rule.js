2b56f76d62296f9a48f2778a7ea0e95bb6456db7
Kamal <kamal.jamali63@gmail.com>
2020-11-28 16:21:58 +0330

    fix bug

--- rules/user/permision-rule.js
+++ rules/user/permision-rule.js
@@ -10,75 +10,142 @@ module.exports = Rule;
  * Check data
  */
 Rule.check = function check(user, data, depid) {
+
     return new Promise((resolve, reject) => {
         const User = mongoose.model('User');
         const ObjectId = require("mongoose").Types.ObjectId;
-
-        const pipeline = [
-            {
-                "$match": {
-                    "name": user.userName
+        let pipeline = [];
+        if (depid) {
+            pipeline = [
+                {
+                    "$match": {
+                        "name": user.userName
+                    }
+                },
+                {
+                    "$project": {
+                        "role": "$role_group_role",
+                        "group": "$role_group_group"
+                    }
+                },
+                { "$unwind": "$role" },
+                {
+                    "$lookup": {
+                        "from": "roles",
+                        "localField": "role",
+                        "foreignField": "_id",
+                        "as": "res"
+                    }
+                },
+                {
+                    "$project": {
+                        "role": 1,
+                        "group": 1,
+                        "permision": "$res.permision"
+                    }
+                },
+                {
+                    "$unwind": "$permision"
+                },
+                {
+                    "$unwind": "$permision"
+                },
+                {
+                    "$lookup": {
+                        "from": "rules",
+                        "localField": "permision",
+                        "foreignField": "id",
+                        "as": "res2"
+                    }
+                },
+                {
+                    "$unwind": "$res2"
+                },
+                {
+                    "$project": {
+                        "role": 1,
+                        "group": 1,
+                        "source": "$res2.short_cut"
+                    }
+                },
+                {
+                    "$match": {
+                        $or: [{
+                            "source": data,
+                            "group": ObjectId(depid)
+                        },
+                        {
+                            role: { $in: [ObjectId("5e9d8ba649e7457e91ec1e61"), ObjectId("5e9d8c9649e7457e91ec1f89")] }
+                        }]
+                    }
                 }
-            },
-            {
-                "$project": {
-                    "role": "$role_group_role",
-                    "group": "$role_group_group"
+            ];
+        } else {
+            pipeline = [
+                {
+                    "$match": {
+                        "name": user.userName
+                    }
+                },
+                {
+                    "$project": {
+                        "role": "$role_group_role",
+                        "group": "$role_group_group"
+                    }
+                },
+                { "$unwind": "$role" },
+                {
+                    "$lookup": {
+                        "from": "roles",
+                        "localField": "role",
+                        "foreignField": "_id",
+                        "as": "res"
+                    }
+                },
+                {
+                    "$project": {
+                        "role": 1,
+                        "group": 1,
+                        "permision": "$res.permision"
+                    }
+                },
+                {
+                    "$unwind": "$permision"
+                },
+                {
+                    "$unwind": "$permision"
+                },
+                {
+                    "$lookup": {
+                        "from": "rules",
+                        "localField": "permision",
+                        "foreignField": "id",
+                        "as": "res2"
+                    }
+                },
+                {
+                    "$unwind": "$res2"
+                },
+                {
+                    "$project": {
+                        "role": 1,
+                        "group": 1,
+                        "source": "$res2.short_cut"
+                    }
+                },
+                {
+                    "$match": {
+                        $or: [{
+                            "source": data,
+                        },
+                        {
+                            role: { $in: [ObjectId("5e9d8ba649e7457e91ec1e61"), ObjectId("5e9d8c9649e7457e91ec1f89")] }
+                        }]
+                    }
                 }
-            },
-            { "$unwind": "$role" },
-            {
-                "$lookup": {
-                    "from": "roles",
-                    "localField": "role",
-                    "foreignField": "_id",
-                    "as": "res"
-                }
-            },
-            {
-                "$project": {
-                    "role": 1,
-                    "group": 1,
-                    "permision": "$res.permision"
-                }
-            },
-            {
-                "$unwind": "$permision"
-            },
-            {
-                "$unwind": "$permision"
-            },
-            {
-                "$lookup": {
-                    "from": "rules",
-                    "localField": "permision",
-                    "foreignField": "id",
-                    "as": "res2"
-                }
-            },
-            {
-                "$unwind": "$res2"
-            },
-            {
-                "$project": {
-                    "role": 1,
-                    "group": 1,
-                    "source": "$res2.short_cut"
-                }
-            },
-            {
-                "$match": {
-                    $or: [{
-                        "source": data,
-                        "group": ObjectId(depid)
-                    },
-                    {
-                        role: { $in: [ObjectId("5e9d8ba649e7457e91ec1e61"), ObjectId("5e9d8c9649e7457e91ec1f89")] }
-                    }]
-                }
-            }
-        ];
-
+            ];
+        }
+        /************************ */
         User.aggregate(pipeline)
             .then(res => {
                 res = res || [];
